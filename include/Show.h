1cdb4a53d803a628c77b6a637c8f3b119b231ecd
GSDimitrov22 <GSDimitrov22@codingburgas.bg>
2025-04-23 00:07:07 +0300

    Create the code for Show.h

---
+++ include/Show.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+#include "Movie.h"
+#include "Hall.h"
+
+using namespace std;
+
+class Show {
+public:
+    Movie movie;
+    Hall hall;
+    string time;
+
+    Show(Movie m, Hall h, string t) : movie(m), hall(h), time(t) {}
+};
