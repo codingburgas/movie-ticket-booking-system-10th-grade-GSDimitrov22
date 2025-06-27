5b8760df5dce0c83cf23779dc0e3b23d1ec85a59
GSDimitrov22 <GSDimitrov22@codingburgas.bg>
2025-06-28 02:37:59 +0300

    Create Seat.h

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/ivans/OneDrive/Работен плот/New folder (2)/movie-ticket-booking-system-10th-grade-GSDimitrov22
+# Install script for directory: C:/Users/ivans/OneDrive/Работен плот/MOVIE AND TICKET APP LAST VERSION/movie-ticket-booking-system-10th-grade-GSDimitrov22
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "C:/Users/ivans/OneDrive/Работен плот/New folder (2)/movie-ticket-booking-system-10th-grade-GSDimitrov22/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/ivans/OneDrive/Работен плот/MOVIE AND TICKET APP LAST VERSION/movie-ticket-booking-system-10th-grade-GSDimitrov22/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
---
+++ include/Seat.h
@@ -0,0 +1,8 @@
+//
+// Created by ivans on 28/06/2025.
+//
+
+#ifndef SEAT_H
+#define SEAT_H
+
+#endif //SEAT_H
