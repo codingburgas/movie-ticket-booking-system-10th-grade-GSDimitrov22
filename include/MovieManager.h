681dd260a1526c2049c37b09e3666b05854cb435
GSDimitrov22 <GSDimitrov22@codingburgas.bg>
2025-06-28 02:44:23 +0300

    Update MovieManager.h

--- include/MovieManager.h
+++ include/MovieManager.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <vector>
+#include <string>
 #include "Movie.h"
-
 using namespace std;
 
 class MovieManager {
@@ -16,9 +16,35 @@ public:
     vector<Movie> searchByTitle(const string& title) const {
         vector<Movie> results;
         for (const auto& movie : movies) {
-            if (movie.title == title) {
+            if (movie.title == title)
                 results.push_back(movie);
-            }
+        }
+        return results;
+    }
+
+    vector<Movie> searchByLanguage(const string& language) const {
+        vector<Movie> results;
+        for (const auto& movie : movies) {
+            if (movie.language == language)
+                results.push_back(movie);
+        }
+        return results;
+    }
+
+    vector<Movie> searchByGenre(const string& genre) const {
+        vector<Movie> results;
+        for (const auto& movie : movies) {
+            if (movie.genre == genre)
+                results.push_back(movie);
+        }
+        return results;
+    }
+
+    vector<Movie> searchByReleaseDate(const string& date) const {
+        vector<Movie> results;
+        for (const auto& movie : movies) {
+            if (movie.releaseDate == date)
+                results.push_back(movie);
         }
         return results;
     }
