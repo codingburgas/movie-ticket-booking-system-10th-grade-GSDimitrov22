908738538756ad56da032581aad5a2a8e0be842d
GSDimitrov22 <GSDimitrov22@codingburgas.bg>
2025-06-28 02:39:40 +0300

    Create the code for Hall.h

--- include/Hall.h
+++ include/Hall.h
@@ -1,11 +1,23 @@
 #pragma once
-
+#include <vector>
+#include "Seat.h"
 using namespace std;
 
 class Hall {
 public:
     int hallId;
-    int totalSeats;
+    vector<Seat> seats;
 
-    Hall(int id, int seats) : hallId(id), totalSeats(seats) {}
+    Hall(int id) : hallId(id) {}
+
+    void addSeat(const Seat& seat) {
+        seats.push_back(seat);
+    }
+
+    Seat* getSeat(const string& seatNumber) {
+        for (auto& seat : seats) {
+            if (seat.seatNumber == seatNumber) return &seat;
+        }
+        return nullptr;
+    }
 };
