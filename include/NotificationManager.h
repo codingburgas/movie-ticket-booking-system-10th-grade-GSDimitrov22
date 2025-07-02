2514cef02b9206591e586f8de47c36fb3c229043
GSDimitrov22 <GSDimitrov22@codingburgas.bg>
2025-07-02 08:28:40 +0300

    Update NotificationManager.h

--- include/NotificationManager.h
+++ include/NotificationManager.h
@@ -2,19 +2,13 @@
 #include <iostream>
 #include "Movie.h"
 #include "Booking.h"
-using namespace std;
 
-class NotificationManager {
-public:
+struct NotificationManager {
     static void notifyNewMovie(const Movie& movie) {
-        cout << "\U0001F4E2 New Movie Released: " << movie.title << endl;
+        std::cout << "New movie added: " << movie.title << std::endl;
     }
 
-    static void notifyBooking(const Booking& booking) {
-        cout << "\U0001F4E2 Booking Confirmed for: " << booking.user.name << endl;
-    }
-
-    static void notifyCancellation(const Booking& booking) {
-        cout << "\U0001F4E2 Booking Canceled for: " << booking.user.name << endl;
+    static void notifyBookingConfirmation(const Booking& booking) {
+        std::cout << "Booking confirmed for " << booking.user.name << std::endl;
     }
 };
